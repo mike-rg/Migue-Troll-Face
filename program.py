fd321a3f8deadd4f4a5151ff17d68c029e893d80
migue <miguel.alberto.maldonado@gmail.com>
2015-12-17 15:30:06 -0300

    Verificar manejo de archivos

---
+++ program.py
@@ -0,0 +1,87 @@
+"""
+	Pequenio 'Troll Program'
+"""
+
+import sys
+import argparse
+import textwrap
+import PIL
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+
+# Defincion de posicion del texto en la imagen
+TAMANIO_SANGRIA = 12
+TAMANIO_RENGLON = 8
+# Definicion de delimitadores
+MAXLINEAS = 7
+MAXCHARS_POR_LINEA = 24
+MAXCHARS = 130
+# Importar fuente
+FSIZE = 10
+FTYPE = "/usr/share/fonts/truetype/ubuntu-font-family/UbuntuMono-R.ttf"
+FONT = ImageFont.truetype(FTYPE, FSIZE)
+# Imagen por defecto
+IFILE = "dice.png"
+
+
+MENSAJE_NO_TEXTO = "Ingresa un texto ;)"
+
+
+def dibujar_texto(string):
+	if string == None:
+		string = MENSAJE_NO_TEXTO
+
+	linea = ""
+	X = TAMANIO_SANGRIA
+	Y = TAMANIO_RENGLON
+	imagen = Image.open(IFILE)
+	dibujar = ImageDraw.Draw(imagen)
+	
+	# Procesa el texto
+	while len(string) > 0:
+		t = string.partition(" ")
+		if (len(linea) + len(t[0])) >=  MAXCHARS_POR_LINEA:
+			dibujar.text((X, Y), linea, (0,0,0), font=FONT)
+			Y = Y + TAMANIO_RENGLON
+			linea = ""
+		linea = linea + t[0] + " "
+		string = t[2]
+
+	# Dibuja la ultima linea menor a MAXCHARS_POR_LINEA
+	dibujar.text((X,Y), linea, (0,0,0), font=FONT)
+
+	dibujar = ImageDraw.Draw(imagen)
+	imagen.save("escribido.png")
+
+
+def _checkear_texto(string):
+	if len(string) > MAXCHARS:
+		mensaje = "El texto supera la cantidad de caracteres aceptados."
+		raise argparse.ArgumentTypeError(mensaje)
+	return string
+
+
+def main():
+	epilogmsj = '''\
+			Advertencia:
+			-----------------------------------
+			Se imprime solo el argumento 'hola'
+			Para:
+				ie. program.py -i hola
+			'''
+	
+	parser = argparse.ArgumentParser(description='--- El Migue dice ---',
+									 formatter_class=argparse.RawDescriptionHelpFormatter,
+									 epilog=textwrap.dedent(epilogmsj))
+	
+	parser.add_argument('-i', type=_checkear_texto,
+		                      help='''Texto de entrada. 
+		                              ie. program.py -i "Hola mundo''')
+	args = parser.parse_args()
+	texto = args.i
+	dibujar_texto(texto)
+
+
+if __name__ == "__main__":
+    main()
