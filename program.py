5e9b73129103ab485c4dc4c6c2563c1c5d7506d0
migue <miguel.alberto.maldonado@gmail.com>
2015-12-18 18:36:19 -0300

    Se corrige codigo e identacion

--- program.py
+++ program.py
@@ -1,11 +1,12 @@
 """
-	Pequenio 'Troll Program'
+        Pequenio 'Troll Program'
 """
 
 import sys
 import argparse
 import textwrap
 import PIL
+import string
 from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
@@ -25,63 +26,61 @@ FONT = ImageFont.truetype(FTYPE, FSIZE)
 IFILE = "dice.png"
 
 
-MENSAJE_NO_TEXTO = "Ingresa un texto ;)"
+MJE_NONE_TEXTO = "Ingresa un texto ;)"
 
 
 def dibujar_texto(string):
-	if string == None:
-		string = MENSAJE_NO_TEXTO
-
-	linea = ""
-	X = TAMANIO_SANGRIA
-	Y = TAMANIO_RENGLON
-	imagen = Image.open(IFILE)
-	dibujar = ImageDraw.Draw(imagen)
-	
-	# Procesa el texto
-	while len(string) > 0:
-		t = string.partition(" ")
-		if (len(linea) + len(t[0])) >=  MAXCHARS_POR_LINEA:
-			dibujar.text((X, Y), linea, (0,0,0), font=FONT)
-			Y = Y + TAMANIO_RENGLON
-			linea = ""
-		linea = linea + t[0] + " "
-		string = t[2]
-
-	# Dibuja la ultima linea menor a MAXCHARS_POR_LINEA
-	dibujar.text((X,Y), linea, (0,0,0), font=FONT)
-
-	dibujar = ImageDraw.Draw(imagen)
-	imagen.save("escribido.png")
+    if string is None:
+        string = MJE_NONE_TEXTO
+    linea = ""
+    X = TAMANIO_SANGRIA
+    Y = TAMANIO_RENGLON
+    imagen = Image.open(IFILE)
+    dibujar = ImageDraw.Draw(imagen)
+    # Procesa el texto
+    while len(string) > 0:
+        t = string.partition(" ")
+        if len(linea) + len(t[0]) >= MAXCHARS_POR_LINEA:
+            dibujar.text((X, Y), linea, (0, 0, 0), font=FONT)
+            Y = Y + TAMANIO_RENGLON
+            linea = ""
+        linea = linea + t[0] + " "
+        string = t[2]
+    # Dibuja la ultima linea menor a MAXCHARS_POR_LINEA
+    dibujar.text((X, Y), linea, (0, 0, 0), font=FONT)
+    dibujar = ImageDraw.Draw(imagen)
+    imagen.save("escribido.png")
 
 
 def _checkear_texto(string):
-	if len(string) > MAXCHARS:
-		mensaje = "El texto supera la cantidad de caracteres aceptados."
-		raise argparse.ArgumentTypeError(mensaje)
-	return string
+    if len(string) > MAXCHARS:
+        mensaje = 'El texto supera la cantidad de caracteres aceptados.'
+        raise argparse.ArgumentTypeError(mensaje)
+    return string
 
 
 def main():
-	epilogmsj = '''\
-			Advertencia:
-			-----------------------------------
-			Se imprime solo el argumento 'hola'
-			Para:
-				ie. program.py -i hola
-			'''
-	
-	parser = argparse.ArgumentParser(description='--- El Migue dice ---',
-                                         formatter_class=argparse.RawDescriptionHelpFormatter,
-					 epilog=textwrap.dedent(epilogmsj))
-	
-	parser.add_argument('-i', type=_checkear_texto,
-                                  help='''Texto de entrada. 
-		                          ie. program.py -i "Hola mundo''')
-	args = parser.parse_args()
-	texto = args.i
-	dibujar_texto(texto)
+    epilogmsj = '''
+    Advertencias:
+    -----------------------------------
+    Se imprime solo el argumento 'hola'
+    Para:
+    ie. program.py -i hola
 
+    Cantidad maxima de caracteres = 24
+
+    '''
+    epilog_format = argparse.RawDescriptionHelpFormatter
+    parser = argparse.ArgumentParser(description='--- El migue dice ---',
+                                     formatter_class= epilog_format,
+                                     epilog=epilogmsj)
+    parser.add_argument('-i', type=_checkear_texto,
+                        help='''Texto de entrada.
+                        ie. program.py -i "Hola mundo''')
+    args = parser.parse_args()
+    texto = args.i
+    dibujar_texto(texto)
+    sys.exit()
 
 if __name__ == "__main__":
     main()
